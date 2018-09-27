f9eab12840881f2fd1ffd6d3a8e1bc1e5493bf79
Thai Mao <thaimao@Thais-MBP.home>
2018-09-27 21:17:40 +0100

    Add application state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 class App extends Component {
+  state = {
+    data: {},
+    loading: true
+  };
+
   render() {
     return <div>Hello, Weather App!</div>;
   }
